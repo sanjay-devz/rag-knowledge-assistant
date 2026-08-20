36199f39882532bf151293d1956e46b6af8661b0
sanjay-devz <sanjay.gridz@gmail.com>
2026-08-20 22:35:26 +0530

    PDF name changed changed to check wheter the function is called

--- Src/main.py
+++ Src/main.py
@@ -1,6 +1,6 @@
 from Document_loader import load_pdf
 
 
-text = load_pdf("Data\Documents\50 Gen AI Product Ideas E2E.pdf")
+text = load_pdf("D:\02-GitHub\rag-knowledge-assistant\Data\Documents\50 Gen AI Product Ideas E2E.pdf")
 
 print(text[:3000])

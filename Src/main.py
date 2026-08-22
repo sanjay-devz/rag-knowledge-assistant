e672b14870ccc8a3203f76f85b75af984b1d7dfc
sanjay-devz <sanjay.gridz@gmail.com>
2026-08-22 23:11:25 +0530

    PDF name changed

--- Src/main.py
+++ Src/main.py
@@ -1,6 +1,16 @@
 from document_loader import load_pdf
+from text_chunker import chunk_text
 
 
-text = load_pdf("50 Gen AI Product Ideas E2E.pdf")
+pdf_path = "D:\02-GitHub\rag-knowledge-assistant\Src\50 Gen AI Product Ideas E2E.pdf"
 
-print(text[:3000])
+text = load_pdf(pdf_path)
+
+chunks = chunk_text(text)
+
+print(f"Total characters: {len(text)}")
+print(f"Total chunks: {len(chunks)}")
+
+for i, chunk in enumerate(chunks[:5], start=1):
+    print(f"\n--- Chunk {i} ---")
+    print(chunk)
